33bd99e12cfe6857108a75d74be5eac9e6292dc2
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-07 17:01:39 -0300

    Update Ejercicio-1.pas

--- Ejercicio-1.pas
+++ Ejercicio-1.pas
@@ -1,17 +1,17 @@
-program ejercicio1;
-
-var
-i: integer;
-n, suma, promedio: double;
-
+program pry2;
+var 
+N1, N2, suma, producto: double;
 begin
-suma := 0;
-for i:= 0 to 9 do 
-    begin 
-        write ('Ingrese un numero: ');
-        readln (n);
-        suma := n + suma;
-    end;
-  promedio := suma / 10;
-  writeln('La suma de los valores da ', suma:0:2, ' y el promedio ', promedio:0:2);
+  writeln ('Ingrese el primer valor: ');
+  readln (N1);
+  writeln ('Ingrese el segundo valor: ');
+  readln (N2);
+  suma:= N1 + N2;
+  writeln ('Ingrese el primer valor: ');
+  readln (N1);
+  writeln ('Ingrese el segundo valor: ');
+  readln (N2);
+  producto := N1 * N2; 
+  writeln ('La suma de los primeros dos valores da ', suma:0:1);
+  writeln ('El producto de los ultimos dos valores da ', producto:0:1);
 end.
