bb1f7eb36c400c99e843c68269a464cac3baaf0d
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-07 17:24:34 -0300

    Create Ejercicio-3.pas

---
+++ Ejercicio-3.pas
@@ -0,0 +1,11 @@
+program pry3;
+var
+ sueldo: Double;
+begin
+ Write('Ingrese el sueldo:');
+ readln(sueldo);
+ if sueldo > 3000 then
+    write ('Esta persona debe abonar impuestos')
+    else 
+    write ('Esta persona no debe abonar impuestos');
+end.
