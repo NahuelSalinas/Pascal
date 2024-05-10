ecab6bbd8ae297411366a6c0d5aaf2ebd2f6366d
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-10 17:37:05 +0200

    Update pry10.pas

--- pry10.pas
+++ pry10.pas
@@ -1,24 +1,12 @@
 program pry10;
-
-var 
-    N1, N2 : double;
-
+var N1, N2 : double;
 begin 
     writeln ('Ingrese dos numeros: ');
     readln (N1, N2);
-    if N1 = N2 then
-        begin
-            writeln ('Son iguales.');
-        end
-    else
-        begin
-        if N1 > N2 then
-            begin
-                writeln (N1:0:0, ' es mayor.');
-            end 
-            else
-                begin
-                    writeln (N2:0:0, ' es mayor.') 
-                end
-        end;
+        if N1 = N2 then
+            writeln ('Son iguales.')
+        else if N1 > N2 then
+            writeln (N1:0:1, ' es mayor.')
+        else 
+            writeln (N2:0:1, ' es mayor.');
 end.
