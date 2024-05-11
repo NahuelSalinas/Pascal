51d990dd3d481a7fb9854564d02e1f1e2f308222
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-11 18:10:17 +0200

    Update pry18.pas

--- pry18.pas
+++ pry18.pas
@@ -1,16 +1,13 @@
 program pry18;
-
 var
   nPreguntas, nPAprobadas: Integer;
   porcentaje: Double;
-
 begin
   Write('Ingrese la cantidad de preguntas: ');
   ReadLn(nPreguntas);
   Write('Ingrese la cantidad de preguntas aprobadas: ');
   ReadLn(nPAprobadas);
   porcentaje:= (nPAprobadas * 100) / nPreguntas;
-  
   if (nPreguntas < nPAprobadas) then
     writeln ('ERROR: La cantidad total de preguntas no puede ser menor a las preguntas aprobadas.')
   else if (porcentaje >= 90) then
