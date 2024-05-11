a6aab1d4de2714800e3981a54a99caae1c4abbc5
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-11 16:49:08 +0200

    Update pry11.pas

--- pry11.pas
+++ pry11.pas
@@ -1,26 +1,20 @@
 program pry11;
-
 var 
-    N1, N2 : integer;
-    division : double; 
+    N1, N2, producto : integer;
+    divid : double; 
 begin 
     writeln ('Ingrese dos numeros: ');
     readln (N1, N2);
-    
     if N1 = N2 then
-        begin
-            writeln ('Son iguales.');
-        end
+        writeln ('Son iguales.')
+    else if N1 > N2 then
+    begin
+        writeln ('Su suma da ', N1 + N2, ' y su diferencia ', N1 - N2);
+    end
     else
-        begin
-        if N1 > N2 then
-            begin
-                writeln ('Su suma da ', N1 + N2, ' y su diferencia ', N1 - N2);
-            end 
-            else
-                begin
-                division := N1 / N2;
-                    writeln ('Su producto da ', N1 * N2, ' y su división ', division:0:1);
-                end
-        end;
+    begin
+        divid := N1 / N2;
+        producto := N1 * N2;
+        writeln ('Su producto da ', producto, ' y su división ', divid);
+    end;
 end.
