b6a78eee5165ca39ff5413c80c111d4b7881dcbc
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-11 17:09:17 +0200

    Update pry13.pas

--- pry13.pas
+++ pry13.pas
@@ -1,16 +1,10 @@
 program pry13;
-
-var
-    numero: integer;
-    
+var numero: integer;
 begin
   writeln ('Ingrese un número entero del 1 al 99: ');
   read (numero);
   if numero < 10 then 
-    begin
-        writeln ('El número tiene un (1) digito.');
-    end
+        writeln ('El número tiene un (1) digito.')
     else
         writeln ('El número tiene dos (2) digitos.');
 end.
-
