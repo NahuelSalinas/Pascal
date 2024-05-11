b5ba6b9367ecd1f68810750ee6f2f717c804fc20
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-11 17:13:42 +0200

    Update pry14.pas

--- pry14.pas
+++ pry14.pas
@@ -1,25 +1,13 @@
 program pry14;
-
-var
-    promedio, n1, n2, n3: double;
-    
+var promedio, n1, n2, n3: double;
 begin
   writeln ('Ingrese tres notas del alumno: ');
   read (n1, n2, n3);
   promedio:= (n1 + n2 + n3) / 3;
   if (promedio < 4) then 
-    begin
-        writeln ('Reprobado');
-    end
-    else 
-        if (promedio >= 7) then
-            begin
-                writeln ('Promocionado');
-            end
-                else 
-                    if (promedio >= 4) and (promedio < 7) then
-                        begin
-                            writeln ('Regular');
-                        end
-            end.
+    writeln ('Reprobado')
+    else if (promedio >= 7) then
+        writeln ('Promocionado')
+    else if (promedio >= 4) and (promedio < 7) then
+        writeln ('Regular');
 end.
