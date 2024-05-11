9404e129086a991eb543b002ead2d7856ae2a4ad
Nahuel Salinas <90853818+NahuelSalinas@users.noreply.github.com>
2024-05-11 17:42:33 +0200

    Update pry17.pas

--- pry17.pas
+++ pry17.pas
@@ -1,20 +1,17 @@
 program pry17;
-
 var
   numero: Integer;
-
 begin
-  Write('Ingrese un numero entero positivo (hasta 999): ');
-  ReadLn(numero);
-
+  write('Ingrese un numero entero positivo (hasta 999): ');
+  readln(numero);
   if (numero < 0) then
-    WriteLn('Error: Por favor, ingrese un número positivo.');
+    writeLn('Error: Por favor, ingrese un número positivo.');
   else if (numero > 999) then
-    WriteLn('Error: El numero ingresado tiene más de tres cifras.');
+    writeln('Error: El numero ingresado tiene más de tres cifras.');
   else if (numero < 10) then
-    WriteLn('El numero tiene 1 cifra.');
+    writeln('El numero tiene 1 cifra.');
   else if (numero < 100) then
-    WriteLn('El numero tiene 2 cifras.');
+    writeln('El numero tiene 2 cifras.');
   else
-    WriteLn('El numero tiene 3 cifras.');
+    writeln('El numero tiene 3 cifras.');
 end.
